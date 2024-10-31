33143cb1c5208d75a4a35f3e08b82c4d48baa0e2
Arthur Grandi <103533086+ArthurGrandi@users.noreply.github.com>
2024-10-31 22:04:58 +0000

    triggers

--- 09_05_Aula3_trabalho_BD1/main_inserts_selects_joins_views_procedures.sql
+++ 09_05_Aula3_trabalho_BD1/main_inserts_selects_joins_views_procedures.sql
@@ -436,3 +436,27 @@ JOIN
     funcionarios f ON p.idFuncionario = f.idFuncionario;
 
 SELECT * FROM vw_pedidos_funcionarios;
+
+------ CODIGOS TRIGGERS
+
+	CREATE OR REPLACE FUNCTION log_conclusao_pedido()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Verifica se o status do pedido inserido é "Concluído"
+    IF NEW.status = 'Concluído' THEN
+        -- Registro de mensagem no log para sinalizar que o pedido foi concluído
+        RAISE NOTICE 'Pedido % foi concluído com sucesso para o cliente %', NEW.idPedido, NEW.idCliente;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Criação da Trigger
+
+CREATE TRIGGER trigger_conclusao_pedido
+AFTER INSERT ON pedidos
+FOR EACH ROW
+EXECUTE FUNCTION log_conclusao_pedido();
+
+INSERT INTO pedidos (idPedido, Data, Status, idCliente, idFuncionario)
+VALUES (11, '2024-10-31', 'Concluído', 1, 2);
